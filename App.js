6c0bba275f495b3a78ff99ba291bc6e664cec0ea
Abdulaziz Alzamil <husamabdulaziz2002@gmail.com>
2024-04-29 21:38:33 +0300

    Fix code format

--- App.js
+++ App.js
@@ -1,25 +1,21 @@
-import { StyleSheet, View } from 'react-native';
-import * as eva from '@eva-design/eva';
-import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
+import { StyleSheet, Text, View } from "react-native";
 
-import MainPage from './pages/MainPage';
-import RoomState from './pages/RoomState';
+import MainPage from "./pages/MainPage";
+import RoomState from "./pages/RoomState";
 
 export default function App() {
   return (
-     <View style={styles.container}>
-    
-        <RoomState />
-     </View>
-    
+    <View style={styles.container}>
+      <RoomState />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#2E2E3E',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#2E2E3E",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- pages/RoomState.tsx
+++ pages/RoomState.tsx
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 import { createStackNavigator } from '@react-navigation/stack';
 import { useState } from 'react';
-import { Text, View, StyleSheet, Alert, TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import { MotiView } from 'moti';
 import { Easing } from 'react-native-reanimated';
@@ -12,64 +12,34 @@ import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { faClock } from '@fortawesome/free-solid-svg-icons/faClock'
 
 import globalStyles from '../globalStyles';
-import { Button } from 'react-native-elements';
-
-const Stack = createStackNavigator();
-
 
 const RoomState = () => {
-    const [index, setIndex] = useState(0);
-    const [sockets, setSockets] = useState(['Socket #1', 'Socket #2', 'Socket #3', 'Socket #4']);
+  const [index, setIndex] = useState(0);
+  const [sockets, setSockets] = useState(['Socket #1', 'Socket #2', 'Socket #3', 'Socket #4']);
 
-return (
-    //<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <View style={[StyleSheet.absoluteFill, {marginTop: 70, gap: 20 }]}>
-
-<View style={styles.headerContainer}>
-        {/* <Image
-          style={[styles.image, {  width: 40, height: 40 }]}
-          source={require("../assets/imgs/logo.png")}
-          contentFit="cover"
-          transition={1000}
-        /> */}
-        <Button
-            type='clear'
-            icon={
-              <Icon
-                 
-                name="chevron-left"
-                size={21}
-                color="#ABE505"
-                
-              />
-            }
-            
-            
-        />
-        <Text
-          style={[styles.headerText]}
-        >Living Room</Text>
+  return (
+    <View style={[StyleSheet.absoluteFill, { marginTop: 70, gap: 20 }]}>
+      <View style={styles.headerContainer}>
+        <Text>{'<'}</Text>
+        <Text style={styles.headerText}>Hamza's House</Text>
         <Image
-          style={[styles.image, {  width: 40, height: 40 }]}
+          style={[styles.image, { width: 40, height: 40 }]}
           source={require("../assets/imgs/logo.png")}
           contentFit="cover"
           transition={1000}
         />
         
       </View>
-
-        <Tab
+      <Tab
         scrollable={sockets.length > 3}
         value={index}
         onChange={(e) => setIndex(e)}
-        //style={{marginTop:100}}
         indicatorStyle={{
           backgroundColor: globalStyles.colors.primary,
           height: 3,
-          
         }}
         variant="default"
-        >
+      >
         {sockets.map((category, index) => (
           <Tab.Item
             key={index}
@@ -77,154 +47,81 @@ return (
             titleStyle={{ fontSize: 16, color: '#fff' }}
           />
         ))}
-        </Tab>
-        
-        <TabView value={index} onChange={setIndex} animationType="spring">
-
-          
-
-            {sockets.map((room, index) => {
-               return (
-                <TabView.Item style={styles.tabViewItem} key={index}>
-                  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-
-                  <View style={[StyleSheet.absoluteFill, {marginTop: 70, gap: 20 }]}>
-                    <View style={[{flexDirection: 'row', justifyContent: 'space-between', marginHorizontal: 40}]}>
-
-                    
-
-                      <Button
-                        type='clear'
-                        icon={
-                          <Icon   
-                            name="bullseye"
-                            size={35}
-                            color="#ABE505"   
-                          />
-                        }    
-                      />
-
-                      <Button
-                        type='clear'
-                        icon={
-                          <FontAwesomeIcon   
-                            icon={faClock}
-                            size={35}
-                            color="#ABE505"   
-                          />
-                        }    
-                      />
-                  </View>
-                  </View>
-                    
-
-                    <View style={[styles.circle, styles.center, {zIndex:5}]}>
-                      
-                      <Button
-                        
-                        type='clear'
-                          icon={
-                            <Icon    
-                              name="power-off"
-                              size={30}
-                              color="#ABE505"
-                                  
-                            />
-                              }
-                              
-                              
-                          />
-
-                      </View>
-                    <View style ={[styles.circle, styles.center, ]}>
-                    {[...Array(3).keys()].map((index)  => {
-                        return(
-                            <MotiView
-                            from = {{opacity:0.6, scale:1}}
-                            animate={{opacity: 0, scale: 1.5}}
-                            transition={{
-                                type: 'timing',
-                                duration: 1500,
-                                easing: Easing.out(Easing.ease),
-                                delay: index * 400,
-                                repeatReverse: false,
-                                loop: true,
-
-                            }}
-                            key={index} 
-                            style={[StyleSheet.absoluteFillObject, styles.circle, {backgroundColor:'#ABE505'}]} 
-                            />
-                        ) 
-                    })}
-                    
-                    </View>
-                </View>
-
-                </TabView.Item>
-              ) 
-            })}
-            
-        </TabView>
+      </Tab>
+      <TabView value={index} onChange={setIndex} animationType="spring">
+        {sockets.map((room, index) => (
+          <TabView.Item style={styles.tabViewItem} key={index}>
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+              <View style={[styles.circle, styles.center, { zIndex: 5 }]} />
+              <View style={[styles.circle, styles.center]}>
+                {[...Array(3).keys()].map((index) => (
+                  <MotiView
+                    from={{ opacity: 0.6, scale: 1 }}
+                    animate={{ opacity: 0, scale: 1.5 }}
+                    transition={{
+                      type: 'timing',
+                      duration: 1500,
+                      easing: Easing.out(Easing.ease),
+                      delay: index * 400,
+                      repeatReverse: false,
+                      loop: true,
+                    }}
+                    key={index}
+                    style={[StyleSheet.absoluteFillObject, styles.circle, { backgroundColor: '#ABE505' }]}
+                  />
+                ))}
+              </View>
+            </View>
+          </TabView.Item>
+        ))}
+      </TabView>
     </View>
-)
-   
+  );
 };
 
 const styles = StyleSheet.create({
-    
-    image: {
-        width: 40,
-        height: 40,
-        borderRadius: 20,
-      },
-      headerText: {
-        fontWeight: 'bold',
-        fontSize: 20,
-        alignSelf: 'flex-end',
-        color: '#fff',
-        lineHeight: 40,
-      },
-    headerContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        gap: -20,
-        marginHorizontal: 20,
-      },    
-    circle: {
-        position: 'absolute',
-        width: 250,
-        height: 250,
-        borderRadius: 125,
-        backgroundColor: '#3A3A4E',
-        shadowColor: '#000',
-        shadowOffset: {
-            width: 0,
-            height: 4,
-        },
-        shadowOpacity: 0.3,
-        shadowRadius: 4,
-        elevation: 5,
+  image: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+  },
+  headerText: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    alignSelf: 'flex-end',
+    color: '#fff',
+    lineHeight: 40,
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    gap: -20,
+    marginHorizontal: 20,
+  },
+  circle: {
+    position: 'absolute',
+    width: 250,
+    height: 250,
+    borderRadius: 125,
+    backgroundColor: '#3A3A4E',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 4,
     },
-    animatedCircle: {
-        backgroundColor: '#ABE505', // Color for animated circle
-    },
-    center: {
-        
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    linearGradient: {
-        flex: 1,
-        paddingLeft: 15,
-        paddingRight: 15,
-        borderRadius: 5
-      },
-      tabViewItem: {
-        backgroundColor: globalStyles.colors.background,
-        width: '100%'
-      },
-
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  center: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  tabViewItem: {
+    backgroundColor: globalStyles.colors.background,
+    width: '100%',
+  },
 });
 
 export default RoomState;
